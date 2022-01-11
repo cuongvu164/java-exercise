5dba282c7d4598476bf9c43e94078e3d4e6b4427
cuongvu164 <cuongmk288@gmail.com>
2022-01-11 16:22:20 +0700

    assignment_301

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     public static void main(String argv[]) {
@@ -10,16 +10,16 @@ public class Main {
 //            choose = ;
             switch (sc.nextInt()) {
                 case 1:
-                    m.DatatypeAndOperators();
+                    m.SumAverageRunningInt(1, 100);
                     break;
                 case 2:
-                    m.LogicalOperators(101, 122, 123, 111);
+                    m.ArrayContains();
                     break;
                 case 3:
-                    m.ArithmeticOperators(121, 12, 123, 22, 23);
+                    m.FrequentNumber();
                     break;
                 case 4:
-                    m.CylinderComputation(4.4f,10);
+                    m.ArrayReverse();
                     break;
                 default:
                     sc.close();
@@ -30,35 +30,87 @@ public class Main {
 
     }
 
-    public void DatatypeAndOperators() {
-        System.out.println((101 + 0) / 3);
-        System.out.println((3.0e-6 * 10000000.1));
-        System.out.println((true && true));
-        System.out.println((false && true));
-        System.out.println(((false && false) || (true && true)));
-        System.out.println((false || false) && (true && true));
+    private void ArrayReverse() {
+        Scanner sc = new Scanner(System.in);
+        List<Integer> arrayList = new ArrayList<>();
+        List<Integer> reversedList = new ArrayList<>();
+
+        System.out.println("Enter length of array ");
+        int len = sc.nextInt();
+
+        System.out.print("Enter item of array:");
+        for (int i = 0; i < len; i++) {
+            arrayList.add(sc.nextInt());
+        }
+        System.out.println("Original Array: " + arrayList);
+        Collections.reverse(arrayList);
+        System.out.println("Reversed Array: " + arrayList);
     }
 
-    public void LogicalOperators(int firstNumber, int secondNumber, int thirdNumber, int fourNumber) {
-        if (firstNumber == secondNumber && secondNumber == thirdNumber && thirdNumber == fourNumber) {
-            System.out.println("Numbers are equal.");
-        } else {
-            System.out.println("Numbers are not equal!");
+    private void FrequentNumber() {
+        int i = 0, count = 0;
+        List<Integer> newArray = new ArrayList<>();
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of array ");
+        int len = sc.nextInt();
+
+        Integer[] array = new Integer[len];
+        System.out.print("Enter item of array:");
+        do {
+            array[i] = sc.nextInt();
+            i++;
+        } while (i < len);
+
+        System.out.print("Enter value:");
+        int value = sc.nextInt();
+        for (int j = 0; j < array.length; j++) {
+            if (array[j] == value) {
+                count++;
+                newArray.add(j);
+            }
+        }
+
+        System.out.println("Amount of frequence: " + count);
+        System.out.println("Indexs: " + newArray);
+    }
+
+    private void ArrayContains() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter length of array ");
+        int n = sc.nextInt();
+
+        String[] stringArray = new String[n];
+        System.out.print("Enter item of array:");
+        for (int i = 0; i < n; i++) {
+            stringArray[i] = sc.next();
+        }
+//        Arrays.stream(stringArray).forEach(item -> System.out.println(item));
+        System.out.println("Enter a string variable ");
+        String sValue = sc.next();
+
+        for (int i = 0; i < stringArray.length; i++) {
+            if (stringArray[i].equals(sValue)) {
+                System.out.println("Check " + sValue + "in Array: Contained!");
+                return;
+            } else {
+                System.out.println("Check '" + sValue + "' in Array: No Contain!");
+                return;
+            }
         }
     }
 
-    public void ArithmeticOperators(int firstNumber, int secondNumber, int thirdNumber, int fourNumber, int fiveNumber) {
-        System.out.println("The sum is " + (firstNumber + secondNumber + thirdNumber + fourNumber + fiveNumber));
+    private void SumAverageRunningInt(int firstNumber, int lastNumber) {
+        int sum;
+        List<Integer> array = new ArrayList<>();
+        for (int i = firstNumber; i <= lastNumber; i++) {
+            array.add(i);
+        }
+        sum = array
+                .stream()
+                .reduce(0, (sub, ele) -> sub + ele);
+        System.out.println("Average: " + (sum / array.size()));
     }
 
-    public void CylinderComputation(float radius, float height){
-        double baseArea = 2*Math.PI*Math.pow(radius,2);
-        double surfaceArea = baseArea + 2*Math.PI*radius*height;
-        double volume = Math.PI*Math.pow(radius,2)*height;
-        System.out.println("Surface area = " + surfaceArea);
-        System.out.println("Base area  = " + baseArea);
-        System.out.println("Volume  = " + volume);
-    }
 
     public static void menu() {
         System.out.println("=====================================================");
